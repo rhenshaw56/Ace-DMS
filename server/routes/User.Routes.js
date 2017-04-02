69f400465ee16cc61e2d4d64646ea94bf1c8ae5b
rowland henshaw <rowland.henshaw@andela.com>
2017-04-02 18:27:19 +0100

    feature(jwt-signup): refactored route

--- server/routes/User.Routes.js
+++ server/routes/User.Routes.js
@@ -3,7 +3,7 @@ import UserMiddleware from '../middlewares/Users';
 import UserController from '../controllers/User.Controller';
 
 const UserRoutes = (router) => {
-  router.route('/api/signup/')
+  router.route('/api/users/signup/')
     .post(UserMiddleware.validateOnCreate, UserController.createUser);
 };
 export default UserRoutes;
