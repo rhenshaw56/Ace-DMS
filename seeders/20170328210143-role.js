09dd3147338b1de379a7677f9a330c9ef709c98a
rowland henshaw <rowland.henshaw@andela.com>
2017-04-12 04:37:26 +0100

    bug(models/test): testing travis build

--- seeders/20170328210143-role.js
+++ seeders/20170328210143-role.js
@@ -12,7 +12,7 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
-    return queryInterface.bulkInsert('Role', [
+    return queryInterface.bulkInsert('Roles', [
       {
         title: 'admin',
         createdAt: new Date(),
@@ -36,6 +36,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('Person', null, {});
     */
-    return queryInterface.bulkDelete('Role', null, {});
+    return queryInterface.bulkDelete('Roles', null, {});
   }
 };
