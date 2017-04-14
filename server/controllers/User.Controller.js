fec88c948651c773949eca96a76a92aaf4ca900b
rowland henshaw <rowland.henshaw@andela.com>
2017-04-14 06:37:54 +0100

    feature(controllers): added controllers to handle requests

--- server/controllers/User.Controller.js
+++ server/controllers/User.Controller.js
@@ -3,7 +3,6 @@ import Auth from '../middlewares/Auth';
 import ErrorHandler from '../helpers/ErrorHandler';
 import ResponseHandler from '../helpers/ResponseHandler';
 
-// const user = db.user;
 
 class UserController {
 
@@ -70,7 +69,7 @@ class UserController {
           } else {
             ResponseHandler.send401(
               response,
-              { message: 'Wrong password!' }
+              { message: 'Bad Login Details!' }
             );
           }
         } else {
@@ -88,7 +87,7 @@ class UserController {
     }
   }
   static logOut(request, response) {
-    const id = request.body.id;
+    const id = request.decoded.userId;
     model.User.findById(id)
     .then((user) => {
       user.update({ currentToken: null })
@@ -99,6 +98,152 @@ class UserController {
           { message: 'Successfully Logged out' }
         );
       });
+    });
+  }
+  static searchUsers(request, response) {
+    const search = request.query.search;
+    const limit = request.query.limit;
+    const offset = request.query.offset;
+    const page = request.query.page;
+    const queryBuilder = {
+      order: '"createdAt" DESC'
+    };
+    if (limit) {
+      queryBuilder.limit = limit;
+    }
+    if (offset) {
+      queryBuilder.offset = offset;
+    }
+    if (page) {
+      // override offset if a page is specified, and default limit is 10
+      const pageLimit = limit || 10;
+      queryBuilder.offset = (page * pageLimit) - pageLimit;
+      queryBuilder.limit = pageLimit;
+    }
+    if (search) {
+      const searchList = search.split(/\s+/);
+      queryBuilder.where = {
+        $or: [{ firstName: { $iLike: { $any: searchList } } },
+        { lastName: { $iLike: { $any: searchList } } },
+        { email: { $iLike: { $any: searchList } } }]
+      };
+    }
+    model.User.findAndCountAll(queryBuilder)
+    .then((users) => {
+      if (users.rows.length > 0) {
+        ResponseHandler.sendResponse(
+          response,
+          200,
+          {
+            users: users.rows
+              .map(user => UserController.formatUserDetails(user)),
+            total: users.count
+          }
+        );
+      } else {
+        ResponseHandler.send404(response);
+      }
+    });
+  }
+  static findUser(request, response) {
+    const userId = Number(request.params.id);
+    model.User.findById(userId)
+    .then((user) => {
+      if (user) {
+        ResponseHandler.sendResponse(
+          response,
+          200,
+          UserController.formatUserDetails(user)
+        );
+      }
+    })
+    .catch((error) => {
+      ErrorHandler.handleRequestError(response, error);
+    });
+  }
+  static updateUserDetails(request, response) {
+    model.User.findById(request.params.id)
+    .then((user) => {
+      if (user) {
+        user.update(request.body)
+        .then((updatedUser) => {
+          ResponseHandler.sendResponse(
+            response,
+            200,
+            UserController.formatUserDetails(updatedUser)
+          );
+        })
+        .catch((error) => {
+          ErrorHandler.handleRequestError(response, error);
+        });
+      } else {
+        ResponseHandler.send404(response);
+      }
+    });
+  }
+
+  static removeUser(request, response) {
+    const userId = Number(request.params.id);
+    model.User.destroy({
+      where: {
+        id: userId
+      }
+    })
+    .then((deletedRow) => {
+      if (deletedRow) {
+        ResponseHandler.sendResponse(
+        response,
+        200,
+        { message: 'User Deleted' }
+      );
+      } else {
+        ResponseHandler.send404(response);
+      }
+    })
+    .catch((error) => {
+      ErrorHandler.handleRequestError(response, error);
+    });
+  }
+
+  static retrieveUserDocuments(request, response) {
+    const userId = Number(request.params.id);
+    const userRoleId = request.decoded.roleId;
+    const retrieverId = request.decoded.userId;
+    model.User.findById(id, {
+      attributes: [
+        'id', 'firstName', 'lastName', 'email', 'roleId'],
+      include: {
+        model: model.Document,
+        attributes: ['id', 'access', 'title', 'content', 'ownerid', 'createdAt']
+      }
+    })
+    .then((user) => {
+      if (user) {
+        const foundDocuments = user.Documents.filter((document) => {
+          if (Auth.verifyAdmin(userRoleId)) {
+            return true;
+          } else if ((document.access === 'public' ||
+                      userRoleId === user.roleId) &&
+                      document.access !== 'private') {
+            return true;
+          } else if (document.access === 'private' &&
+                      document.ownerId === retrieverId) {
+            return true;
+          }
+          return false;
+        });
+        const userDetails = Object.assign(
+          {},
+          UserController.formatUserDetails(user),
+          { documents });
+        ResponseHandler.sendResponse(
+            response,
+            200,
+            userDetails
+          );
+      } else {
+        ResponseHandler.send404(response);
+      }
     });
   }
 }
