8e085c6ba9fce82b32fcbca42efd7f310615804b
rowland henshaw <rowland.henshaw@andela.com>
2017-05-05 11:18:04 +0100

    saving changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
 import userRoutes from './userRoutes';
-
+import swagger from './swagger';
 import documentRoutes from './documentRoutes';
 import roleRoutes from './roleRoutes';
 
 const Routes = (router) => {
+  swagger(router);
   userRoutes(router);
   documentRoutes(router);
   roleRoutes(router);
